6946e331243bf2c50d2582b2a410a8029ba5f1ec
Stefan Seidel <stefan@seidel.engineering>
2021-12-16 19:06:06 +0100

    Add histogram with PCs over Weekdays

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ take a sheet with event logs from the event viewer and compare different PCs
 
 import pandas as pd
 import os
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+sns.set()
 
 os.chdir("D:\\OneDrive\\OneDrive\\Python\\shutdowns\\")
 # df = pd.read_excel("source\\shutdown_table.xlsx")
@@ -17,23 +20,46 @@ excelfile = pd.ExcelFile("source\\shutdown_table.xlsx")
 # df = pd.read_excel('Shutdowns.xlsx', sheet_name = None, header=None)
 # =============================================================================
 df = pd.DataFrame()
-for sheet in excelfile.sheet_names:
+for i, sheet in enumerate(excelfile.sheet_names):
     print(sheet)
     temp_df =  excelfile.parse(sheet_name = sheet, header=None)
     temp_df["PC"] = sheet
+    temp_df["PCnum"] = i
     df = pd.concat([df, temp_df])
-df.columns = ['Level', 'DateAndTime', 'Source', 'EventID', 'TaskCategory', 'PC']
+
+df.columns = ['Level', 'DateAndTime', 'Source', 'EventID', 'TaskCategory', 'PC', 'PCnum']
+
+days = ["Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday", "Sunday"]
+
+df['Weekday'] = df['DateAndTime'].dt.dayofweek
+df['WeekdayName'] = df['Weekday'].apply(lambda x : days[x]) 
+
 df.reset_index(drop=True, inplace=True)
 
+# simple scatter plot of the PCs by name over time
 plt.scatter(x=df['DateAndTime'], y=df['PC'])
 
+# simple cdf of all PCs over time
 PC_names = df['PC'].unique()
-
 dfs = {}
 for name in PC_names:
     dfs[name] = df[df['PC'] == name]
-    
 f, ax = plt.subplots(figsize=(8, 8))
-
 for name, entries_df in dfs.items():
     ax = sns.kdeplot(entries_df['DateAndTime'], cumulative=True)
+    
+# hist plot over weekdays
+for name, entries_df in dfs.items():
+    ax = sns.kdeplot(entries_df['DateAndTime'], cumulative=True)
+    
+    
+# Create histogram over weekdays
+fig, ax = plt.subplots(figsize=(10,4))
+sns.histplot(data=df, x='Weekday', hue='PCnum', multiple='dodge', discrete=True,
+             edgecolor='white', palette=plt.cm.Accent, alpha=1)
+# Additional formatting
+sns.despine()
+ax.get_legend().set_frame_on(False)
+    
+    
